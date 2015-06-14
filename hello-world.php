cf0b98a262ee87c72d32601a654968a0229c4c1f
Juan Manuel Torres <kinojman@yahoo.com>
2015-06-14 06:37:31 +0000

    Remove default c9 hello world message.

--- hello-world.php
+++ hello-world.php
@@ -3,5 +3,6 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo 'Hello world from Cloud9!';
+echo 'Hello, World!';
+
 ?>
